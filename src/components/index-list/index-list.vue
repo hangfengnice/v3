674f2df0d57b37b2e0c16d74986dd1311bfb756e
hangfeng <nicehangfeng@foxmail.com>
2021-04-28 18:39:53 +0800

    fix

---
+++ src/components/index-list/index-list.vue
@@ -0,0 +1,113 @@
+<template>
+  <scroll class="index-list" :probe-type='3' @scroll="onScroll">
+    <ul ref='groupRef'>
+      <li class="group" v-for="group in data" :key="group.title">
+        <h2 class="title">{{group.title}}</h2>
+        <ul>
+          <li class="item" v-for="item in group.list" :key='item.id'>
+            <img v-lazy="item.pic" class="avatar" alt="avatar">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div class="fixed" v-show="fixedTitle">
+      <div class="fixed-title">{{fixedTitle}}</div>
+    </div>
+  </scroll>
+</template>
+
+<script>
+import scroll from '@/components/base/scroll/scroll'
+import useFixed from './use-fixed'
+
+export default {
+  components: { scroll },
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  setup (props) {
+    const { groupRef, onScroll, fixedTitle } = useFixed(props)
+    return {
+      groupRef,
+      onScroll,
+      fixedTitle
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.index-list {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background: $color-background;
+    .group {
+      padding-bottom: 30px;
+      .title {
+        height: 30px;
+        line-height: 30px;
+        padding-left: 20px;
+        font-size: $font-size-small;
+        color: $color-text-l;
+        background: $color-highlight-background;
+      }
+      .item {
+        display: flex;
+        align-items: center;
+        padding: 20px 0 0 30px;
+        .avatar {
+          width: 50px;
+          height: 50px;
+          border-radius: 50%;
+        }
+        .name {
+          margin-left: 20px;
+          color: $color-text-l;
+          font-size: $font-size-medium;
+        }
+      }
+    }
+    .fixed {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      .fixed-title {
+        height: 30px;
+        line-height: 30px;
+        padding-left: 20px;
+        font-size: $font-size-small;
+        color: $color-text-l;
+        background: $color-highlight-background;
+      }
+    }
+    .shortcut {
+      position: absolute;
+      right: 4px;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 20px;
+      padding: 20px 0;
+      border-radius: 10px;
+      text-align: center;
+      background: $color-background-d;
+      font-family: Helvetica;
+      .item {
+        padding: 3px;
+        line-height: 1;
+        color: $color-text-l;
+        font-size: $font-size-small;
+        &.current {
+          color: $color-theme
+        }
+      }
+    }
+  }
+
+</style>
---
+++ src/views/singer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="singer" v-loading='!singers.length'>
+    <IndexList :data='singers' />
+  </div>
+</template>
+
+<script>
+import { getSingerList } from '@/service/singer.js'
+import IndexList from '@/components/index-list/index-list'
+export default {
+  name: 'singer',
+  components: {
+    IndexList
+  },
+  data () {
+    return {
+      singers: []
+    }
+  },
+  async created () {
+    const { singers } = await getSingerList()
+    this.singers = singers
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
+</style>
