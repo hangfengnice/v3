5719dc7f4ac4f86a0947bf13705c09e6703e13a4
hangfeng <nicehangfeng@foxmail.com>
2021-05-15 10:15:15 +0800

    feat

--- src/Vue3.vue
+++ src/Vue3.vue
@@ -1,12 +1,39 @@
 <template>
-  <div>
-    hell owrold
-  </div>
+  <button @click="plus">
+    {{count}}
+  </button>
 </template>
 
 <script>
-export default {
+import { onMounted, reactive, watch } from 'vue'
 
+export default {
+  name: 'vue2-app',
+  data () {
+    return {
+      $data1: 1
+    }
+  },
+  created () {
+    this.$name = 'name'
+  },
+  setup () {
+    const state = reactive({ count: 0 })
+
+    watch(() => state.count, (newval, oldval) => {
+      console.log(newval)
+    })
+
+    onMounted(() => {
+      console.log('h')
+    })
+    onMounted(() => {
+      console.log('h1')
+    })
+    state.count++
+    state.count++
+    state.count++
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 // import App from './App.vue'
-// import Vue3 from './Vue3.vue'
+import Vue3 from './Vue3.vue'
 import router from './router'
 import store from './store'
 import lazyPlugin from 'vue3-lazy'
@@ -8,9 +8,7 @@ import loadingDirective from '@/components/base/loading/directive'
 import noresultDirective from '@/components/base/no-result/directive'
 
 import '@/assets/scss/index.scss'
-createApp({
-  template: '<div>hello</div>'
-})
+createApp(Vue3)
   .use(store)
   .use(router)
   .use(lazyPlugin, {
